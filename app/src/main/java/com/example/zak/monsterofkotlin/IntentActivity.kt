1324423d4117fefda8caa32314b9ae24ac3a7d83
ZakBrinlee <zbrinlee@hotmail.com>
2018-04-16 10:48:17 -0700

    Adding IntentActivity
    Issues with AS so uploading manually

---
+++ app/src/main/java/com/example/zak/monsterofkotlin/IntentActivity.kt
@@ -0,0 +1,38 @@
+package com.example.zak.monsterofkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_intent.*
+
+class IntentActivity : AppCompatActivity() {
+
+    private val TAG = "IntentActivity"
+
+    lateinit var intentView: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate() Restoring previous state")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent)
+
+        //get data from intent
+        val intent = intent
+        val name = intent.getStringExtra("Name")
+
+        //starting debug logging using the logcat
+        Log.d(TAG, "Took in name from intent: " + name)
+
+        //intentView
+        intentView = findViewById(R.id.intentView)
+        //set text
+        val message = "Welcome "+name+""
+        intentView.setText(message)
+        Log.d(TAG, "intentView loaded with intent name. End of onCreate()")
+    }
+
+
+}
